f1b159d2d914190a4c67f2e983b22b85e41db775
gia-ai <garance.grilikhes@efrei.net>
2025-11-06 22:52:25 +0100

    Update cell.h

--- TI_301_PRJ_STUDENTS-master/cell.h
+++ TI_301_PRJ_STUDENTS-master/cell.h
@@ -1,1 +1,14 @@
-// c'est dur
+
+#ifndef CELL_H
+#define CELL_H
+
+typedef struct s_cell{
+  float probability;
+  int arrivee;
+  struct s_cell* next;
+}t_cell;
+
+t_cell* createcell( float, int );
+void displaycell(t_cell);
+
+#endif //CELL_H
