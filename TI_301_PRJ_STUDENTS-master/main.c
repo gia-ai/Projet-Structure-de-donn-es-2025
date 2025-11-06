79a207869e40ba8e69ab4bf497742b2b9130275a
gia-ai <garance.grilikhes@efrei.net>
2025-11-06 22:59:08 +0100

    Update main.c

--- TI_301_PRJ_STUDENTS-master/main.c
+++ TI_301_PRJ_STUDENTS-master/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tableau.h"
+#include "cell.h"
+#include "list.h"
+
 
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    t_list *tab = readGraph("../data/exemple1_from_chatGPT.txt");
+    displayTablist(&tab);
+    //int n = isGraphMakov(*tablist);
 }
