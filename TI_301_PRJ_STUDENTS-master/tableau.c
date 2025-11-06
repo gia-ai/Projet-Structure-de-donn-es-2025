e086fbc9cddb7d3fb3f3ba18aa9a8f1cf25c0ce2
gia-ai <garance.grilikhes@efrei.net>
2025-11-06 23:00:46 +0100

    Create tableau.c

---
+++ TI_301_PRJ_STUDENTS-master/tableau.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include "cell.h"
+#include "list.h"
+#include "tableau.h"
+
+
+
+
+void displayTab( t_list * tab) {
+    int size= sizeof(tab)/sizeof(int);
+    for (int i = 0; i <  size; i ++) {
+        displayList(tab[i]);
+    }
+}
+
+t_list * readGraph(const char *filename) {
+
+    int nb, depart, arrivee;
+    float proba;
+
+    //lecture du fichier texte
+    FILE *file = fopen(filename, "rt"); // read-only, text
+    if (file==NULL)
+    {
+        perror("Could not open file for reading");
+        exit(EXIT_FAILURE);
+    }
+
+    // definition du nombre de sommet
+    fscanf(file, "%d", &nb);
+    {
+        perror("Could not read number of vertices");
+        exit(EXIT_FAILURE);
+    }
+
+    //création du tableau de liste
+    t_list * tab[nb]={NULL}; // taille fixe + initialisation des pointeur à NULL pour éviter toute erreur
+
+    //remplissage du tableau
+    while (fscanf(file, "%d %d %f", &depart, &arrivee, &proba) == 3) //parcours du fichier
+    {
+        if (tab[depart-1]==NULL){ // on regarde si le sommet (la liste associée) est déjà créée dans le tableau
+            tab[depart-1] = createEmptyList(depart); // dépar étant le numéro du sommmet
+        }
+        addHeadList(tab[depart-1], proba, arrivee) ;
+    }
+    fclose(file);
+    return tab;
+}
+
+
+/*
+
+int isGraphMakov(t_tablist tablist) {
+    int temp = 1;
+    for (int i = 0; i <  tablist.size; i ++) {
+        if (tablist.list[i].probatotale >1 || tablist.list[i].probatotale <0.99) {
+            temp = 0;
+            printf("la somme des probabilites du sommet %d est %g \n", i+1, tablist.list[i].probatotale);
+        }
+    }
+    if (temp == 0) {
+        printf("Alors le graphe n est pas un graphe de Markov ");
+    }else {
+        printf("Le graphe est un graphe de Markov ");
+    }
+    return temp;
+}*/
