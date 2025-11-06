76cc4cab9ca41337d69e22ded8480d65c1cdb059
gia-ai <garance.grilikhes@efrei.net>
2025-11-06 22:57:39 +0100

    Update list.h

---
+++ TI_301_PRJ_STUDENTS-master/list.h
@@ -0,0 +1,19 @@
+
+#ifndef LIST_H
+#define LIST_H
+#include "cell.h"
+
+typedef struct s_list {
+    t_cell* head;
+    int number;
+    float probatotale;
+} t_list;
+
+t_list* createEmptyList(int);
+void addHeadList(t_list* , float, int);
+int isEmpltyList(t_list);
+void displayList(t_list);
+
+
+
+#endif //LIST_H
