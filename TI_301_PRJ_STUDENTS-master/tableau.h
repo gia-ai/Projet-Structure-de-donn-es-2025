fc959cc9b7e2936a84c0d9596ed5efa92cf45a04
gia-ai <garance.grilikhes@efrei.net>
2025-11-06 23:02:02 +0100

    Create tableau.h

---
+++ TI_301_PRJ_STUDENTS-master/tableau.h
@@ -0,0 +1,14 @@
+
+#ifndef TABLEAU_H
+#define TABLEAU_H
+#include "list.h"
+
+
+
+void displayTablist( t_list ** );
+t_list * readGraph(const char *filename);
+
+//int isGraphMakov(t_tablist);
+
+
+#endif //TABLEAU_H
