d3a08ded9ad4c0688c5153862b6cb8561956b9c9
gia-ai <garance.grilikhes@efrei.net>
2025-11-13 14:22:39 +0100

    Update pile.h

--- TI_301_PRJ_STUDENTS-master/pile.h
+++ TI_301_PRJ_STUDENTS-master/pile.h
@@ -1,1 +1,20 @@
+#ifndef PILE_H
+#define PILE_H
+#include "tarjan.h"
+#define NBMAX 50 // peut-être à changer pr tab dyn 
 
+
+typedef struct Pile {
+  int values[NBMAX];
+	int nbElts;             
+} 
+
+Pile* creerPileVide();
+t_tarjan_vertex pop(Pile );
+void push(Pile, t_tarjan_vertex);
+int estVidePile(Pile);
+void displayList(Pile);
+
+
+
+#endif //PILE_H
